48b76f27e121b4c4d66adc37416f1fbd08a0a8f9
camden-stamm <camstamm@DESKTOP-VC5CAVT>
2024-02-26 16:46:22 -0500

    Finished Gatsby Tutorial Part 2

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Student01 Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
